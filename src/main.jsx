00620785e45f8210b17f478f7d820a229cdfb389
im-sanny <raselparvezsanny@gmail.com>
2024-06-03 08:12:58 +0600

    axioscommon hook created successfully

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,19 @@ import router from "./routes/Routes";
 import AuthProvider from "./provider/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <AuthProvider>
-        <RouterProvider router={router}></RouterProvider>
-        <Toaster></Toaster>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router}></RouterProvider>
+          <Toaster></Toaster>
+        </AuthProvider>
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
