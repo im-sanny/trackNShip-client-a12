cbf7dba47ea5717618518b211f7cda55982b0b09
im-sanny <raselparvezsanny@gmail.com>
2024-06-09 16:38:13 +0600

    hopage statistics done

--- src/pages/Home/Statistics.jsx
+++ src/pages/Home/Statistics.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Card,
   CardContent,
@@ -5,8 +6,37 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import CountUp from "react-countup";
 
 const Statistics = () => {
+  const axiosSecure = useAxiosSecure();
+  const [totalParcelsBooked, setTotalParcelsBooked] = useState(0);
+  const [totalParcelsDelivered, setTotalParcelsDelivered] = useState(0);
+  const [totalUsers, setTotalUsers] = useState(0);
+
+  useEffect(() => {
+    const fetchStatistics = async () => {
+      try {
+        const response1 = await axiosSecure.get("/allParcel");
+        const response2 = await axiosSecure.get("/user");
+
+        setTotalParcelsBooked(response1.data.length);
+
+        const deliveredParcels = response1.data.filter(
+          (parcel) => parcel.status === "delivered"
+        );
+        setTotalParcelsDelivered(deliveredParcels.length);
+
+        setTotalUsers(response2.data.length);
+      } catch (error) {
+        console.error("Error fetching statistics:", error);
+      }
+    };
+
+    fetchStatistics();
+  }, [axiosSecure]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:mx-20 mb-10">
       <Card className="flex">
@@ -19,7 +49,9 @@ const Statistics = () => {
         </CardDescription>
         <div>
           <CardHeader className="pb-2">
-            <CardTitle className="text-4xl">1,329</CardTitle>
+            <CardTitle className="text-4xl">
+              <CountUp start={0} end={totalParcelsBooked} duration={5} />
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <div className="text-md text-muted-foreground font-semibold">
@@ -38,7 +70,9 @@ const Statistics = () => {
         </CardDescription>
         <div>
           <CardHeader className="pb-2">
-            <CardTitle className="text-4xl">1,329</CardTitle>
+            <CardTitle className="text-4xl">
+              <CountUp start={0} end={totalParcelsDelivered} duration={5} />
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <div className="text-md text-muted-foreground font-semibold">
@@ -57,7 +91,9 @@ const Statistics = () => {
         </CardDescription>
         <div>
           <CardHeader className="pb-2">
-            <CardTitle className="text-4xl">1,329</CardTitle>
+            <CardTitle className="text-4xl">
+              <CountUp start={0} end={totalUsers} duration={5} />
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <div className="text-md text-muted-foreground font-semibold">
