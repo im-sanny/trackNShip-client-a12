dc255d354d57c0a1b6285f1fc9c795ae6599d95b
im-sanny <raselparvezsanny@gmail.com>
2024-07-18 21:51:02 +0600

    update: updated shop logo cards design

--- src/pages/Home/Shop.jsx
+++ src/pages/Home/Shop.jsx
@@ -5,32 +5,32 @@ const Shop = () => {
         <img
           src="https://i.ibb.co/Wy5BBp3/Premade-Retro-70s-Style-Logo-Design-Seventies-Logo-Vintage-Etsy.jpg"
           alt=""
-          className="rounded-3xl h-34 w-full border-2 border-green-400"
+          className="rounded-3xl h-32"
         />
         <img
           src="https://i.ibb.co/WxvfVRS/Play-Station-Store-logo-vector-svg-pdf-formats-free-download-Brandlogos-net.jpg"
           alt=""
-          className="rounded-3xl h-34 w-full"
+          className="rounded-3xl h-32"
         />
         <img
           src="https://i.ibb.co/sjV5HgN/Kids-store-clothing-hanging-from-branch-logo-vector-image-on-Vector-Stock.jpg"
           alt=""
-          className="rounded-3xl h-34 w-full"
+          className="rounded-3xl h-32"
         />
         <img
           src="https://i.ibb.co/gSp138k/The-Gifts-Online-Promotional-Products-Store-Logo-Design.jpg"
           alt=""
-          className="rounded-3xl h-34 w-full"
+          className="rounded-3xl h-32"
         />
         <img
           src="https://i.ibb.co/M8Nffc0/Bird-Gift-Premade-Logo-Design-Customized-with-Your-Business-Name-Ramble-Road-Studios.jpg"
           alt=""
-          className="rounded-3xl h-34 w-full"
+          className="rounded-3xl h-32"
         />
         <img
           src="https://i.ibb.co/J7DfVkn/Buy-Starbucks-Branded-Logo-Svg-Png-online-in-USA.jpg"
           alt=""
-          className="rounded-3xl h-34 w-full"
+          className="rounded-3xl h-32"
         />
       </div>
     </div>
