6b8ec7d33aa2fc4a42c46d12c459536aa9ec4c54
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2024-11-28 22:00:08 +0600

    update: updated some design in registration component

--- src/pages/Authentication/Registration.jsx
+++ src/pages/Authentication/Registration.jsx
@@ -1,39 +1,39 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { FcGoogle } from "react-icons/fc";
-import { Link, useNavigate } from "react-router-dom";
-import Lottie from "lottie-react";
-import bikeCourier from "@/assets/bikeCourier";
-import useAuth from "@/hooks/useAuth";
-import toast from "react-hot-toast";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { imageUpload } from "@/api/utils";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { FcGoogle } from 'react-icons/fc';
+import { Link, useNavigate } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import bikeCourier from '@/assets/bikeCourier';
+import useAuth from '@/hooks/useAuth';
+import toast from 'react-hot-toast';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { imageUpload } from '@/api/utils';
 // import useAxiosSecure from "@/hooks/useAxiosSecure";
 
 // Validation schema
 const schema = yup.object().shape({
-  name: yup.string().required("Name is required"),
-  email: yup.string().email("Invalid email").required("Email is required"),
+  name: yup.string().required('Name is required'),
+  email: yup.string().email('Invalid email').required('Email is required'),
   password: yup
     .string()
-    .min(6, "Password must be at least 6 characters")
-    .required("Password is required"),
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is required'),
   phone: yup
     .string()
-    .matches(/^[0-9]{10,15}$/, "Phone number must be 10-15 digits")
-    .required("Phone number is required"),
+    .matches(/^[0-9]{10,15}$/, 'Phone number must be 10-15 digits')
+    .required('Phone number is required'),
   image: yup
     .mixed()
-    .required("Image is required")
-    .test("fileType", "Unsupported File Format", (value) => {
+    .required('Image is required')
+    .test('fileType', 'Unsupported File Format', (value) => {
       return (
         value &&
         value.length > 0 &&
-        ["image/jpeg", "image/png", "image/gif"].includes(value[0].type)
+        ['image/jpeg', 'image/png', 'image/gif'].includes(value[0].type)
       );
     }),
 });
@@ -46,7 +46,7 @@ const Registration = () => {
     createUser,
     signInWithGoogle,
     updateUserProfile,
-    saveUser
+    saveUser,
   } = useAuth();
 
   const navigate = useNavigate();
@@ -76,8 +76,8 @@ const Registration = () => {
       // Save name, and image in Firebase
       await updateUserProfile(data.name, image_url);
 
-      navigate("/");
-      toast.success("Registration Successful");
+      navigate('/');
+      toast.success('Registration Successful');
       console.log(result);
     } catch (error) {
       console.log(error);
@@ -105,8 +105,8 @@ const Registration = () => {
 
       await saveUser(userInfo);
 
-      navigate("/");
-      toast.success("Registration successful");
+      navigate('/');
+      toast.success('Registration successful');
     } catch (error) {
       console.log(error);
       toast.error(error.message);
@@ -124,57 +124,57 @@ const Registration = () => {
   };
   return (
     <div>
-      <div className="flex justify-center items-center">
-        <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg lg:max-w-4xl">
+      <div className="flex justify-center items-center mt-5">
+        <div className="flex w-full max-w-sm mx-auto overflow-hidden rounded-lg lg:max-w-4xl">
           <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
-            <p className="mt-3 text-xl text-center text-gray-600">
+            <p className="mt-3 text-xl text-center text-muted-foreground font-bold">
               Create an Account It's Free!
             </p>
 
             <div className="flex items-center justify-between mt-4">
               <span className="w-1/5 border-b lg:w-1/4"></span>
 
-              <div className="text-xs text-center text-gray-500 uppercase hover:underline">
+              <div className="text-xs text-center text-muted-foreground uppercase">
                 Registration with Email and Password
               </div>
 
               <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
             </div>
             <form onSubmit={handleSubmit(handleSignUp)}>
-              <div className="mt-4">
+              <div className="mt-4 grid gap-2">
                 <Label htmlFor="name">Username</Label>
                 <Input
                   id="name"
                   autoComplete="name"
                   type="text"
                   placeholder="Enter your name"
-                  {...register("name")}
+                  {...register('name')}
                 />
                 {errors.name && (
                   <p className="text-red-500 text-sm">{errors.name.message}</p>
                 )}
               </div>
-              <div className="mt-4">
+              <div className="mt-4 grid gap-2">
                 <Label htmlFor="email">Email Address</Label>
                 <Input
                   id="email"
                   autoComplete="email"
                   type="email"
                   placeholder="Enter your email"
-                  {...register("email")}
+                  {...register('email')}
                 />
                 {errors.email && (
                   <p className="text-red-500 text-sm">{errors.email.message}</p>
                 )}
               </div>
-              <div className="mt-4">
+              <div className="mt-4 grid gap-2">
                 <Label htmlFor="password">Password</Label>
                 <Input
                   id="password"
                   autoComplete="current-password"
                   type="password"
                   placeholder="********"
-                  {...register("password")}
+                  {...register('password')}
                 />
                 {errors.password && (
                   <p className="text-red-500 text-sm">
@@ -182,26 +182,26 @@ const Registration = () => {
                   </p>
                 )}
               </div>
-              <div className="mt-4">
+              <div className="mt-4 grid gap-2">
                 <Label htmlFor="phone">Phone Number</Label>
                 <Input
                   id="phone"
                   autoComplete="phone"
                   type="tel"
                   placeholder="Enter your phone number"
-                  {...register("phone")}
+                  {...register('phone')}
                 />
                 {errors.phone && (
                   <p className="text-red-500 text-sm">{errors.phone.message}</p>
                 )}
               </div>
-              <div className="mt-4">
+              <div className="mt-4 grid gap-2">
                 <Label htmlFor="image">Image</Label>
                 <Input
                   id="image"
                   type="file"
                   accept="image/*"
-                  {...register("image")}
+                  {...register('image')}
                 />
 
                 {errors.image && (
@@ -209,7 +209,7 @@ const Registration = () => {
                 )}
               </div>
               <Button disabled={loading} type="submit" className="w-full mt-4">
-                {loading ? <span className="loader"></span> : "Register"}
+                {loading ? <span className="loader"></span> : 'Register'}
               </Button>
             </form>
             <Button
@@ -226,7 +226,7 @@ const Registration = () => {
 
               <Link
                 to="/login"
-                className="text-xs text-gray-500 uppercase hover:underline"
+                className="text-xs text-gray-500 uppercase hover:underline hover:text-blue-500"
               >
                 or log in
               </Link>
