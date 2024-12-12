70e92213eef7bb07813c006d3603ce22e8279030
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2024-12-12 12:40:23 +0600

    add: added backgroud color for home page

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,6 +1,6 @@
-import Footer from "@/components/shared/Footer/Footer";
-import Navbar from "@/components/shared/Navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import Footer from '@/components/shared/Footer/Footer';
+import Navbar from '@/components/shared/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
 
 const Root = () => {
   return (
@@ -8,8 +8,10 @@ const Root = () => {
       {/* navbar */}
       <Navbar></Navbar>
       {/* outlet */}
-      <div className="mx-5 lg:mx-10">
-        <Outlet></Outlet>
+      <div className="bg-gradient-to-r from-green-50 via-blue-100 to-blue-200 dark:from-gray-900 dark:via-gray-800 dark:to-gray-700">
+        <div className="mx-5 lg:mx-10">
+          <Outlet></Outlet>
+        </div>
       </div>
       {/* footer */}
       <Footer></Footer>
